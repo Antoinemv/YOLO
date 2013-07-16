c1a1896cc7dfc1729f4c753db226d698f53f79f3
Antoine M.verdier <antoine.magnonverdier@essec.edu>
2013-07-16 17:09:24 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YOLO::Application.config.secret_token = '644a449cd1badf889186eb2e300972557173933f528168b8d8a6ef953a0b3382bb34a9d14ba455fbb502e7dbaedd7f64f21efaa07adc8029cc0754625dc15816'
